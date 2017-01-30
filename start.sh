ab284ab68931f55503a4da3a1819226903ab7c79
root <root@game-host.maas>
2017-01-30 22:39:32 +0000

    forgot to enable auto-updating

--- start.sh
+++ start.sh
@@ -4,11 +4,13 @@ echo " "
 echo " "
 
 echo "Make sure you forwarded ports, the command should like something like the below..."
-echo " docker run -it -p 0.0.0.0:20560:20560/udp -p 0.0.0.0:27015:27015/udp -p 0.0.0.0:7777:7777/udp -p 0.0.0.0:8080:8080 sequel7/killingfloor2"
+echo "docker run -it -p 0.0.0.0:20560:20560/udp -p 0.0.0.0:27015:27015/udp -p 0.0.0.0:7777:7777/udp sequel7/killingfloor2"
 
 echo " "
 echo " "
 
 sleep 6
+
+./steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /opt/server/ +app_update 232130 +quit
 
 WINEDEBUG="fixme-all" unbuffer wine /opt/server/Binaries/Win64/KFServer.exe kf-bioticslab?difficulty=0?maxplayers=20 -port=7777
